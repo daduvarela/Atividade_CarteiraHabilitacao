777c2fdcbf43f4131a55e1751b15b8cc56cffacb
Maria Eduarda varela <madu.varellah@gmail.com>
2022-09-30 16:37:21 -0300

    Aula 13 Atividade Carteira de Habilitação

---
+++ cod.js
@@ -0,0 +1,10 @@
+var idade 
+idade = prompt("Digite a sua idade: ")
+if (idade > 18)
+{
+    document.write ("Você pode tirar carteira de habilitação.")
+}
+else
+{
+    document.write ("Você não pode tirar carteira de habilitação.")
+}
